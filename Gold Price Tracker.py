6921fb22e011dcf87a1e1c3d97836476b94b7ed2
VigNalla <82089701+VigNalla@users.noreply.github.com>
2022-04-26 14:33:42 +0530

    Added Twilio instead of Email

--- Gold Price Tracker.py
+++ Gold Price Tracker.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
 import smtplib
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
-MY_EMAIL = 'warannal@gmail.com'
-PASSWORD = 'Eswari@26'
+account_sid = 'AC723997122710e147b7920e6e7a3ea951'
+auth_token = 'a32647faa50e81b37c3e3470bd54cf21'
+client = Client(account_sid, auth_token)
 DESIRED_PRICE = 5000
 
 URL="https://www.goodreturns.in/gold-rates/"
@@ -20,14 +24,13 @@ current_price = int(item_price)
 
 print(current_price)
 
-# if current_price <= DESIRED_PRICE:
-#     message = f"Price of Gold,is now ₹{current_price}.\n Buy Now!"
-#     connection = smtplib.SMTP('smtp.gmail.com')
-#     connection.starttls()
-#     connection.login(user=MY_EMAIL, password=PASSWORD)
-#     connection.sendmail(
-#         from_addr=MY_EMAIL,
-#         to_addrs='vigneshtnv@outlook.com',
-#         msg=f'Subject:Price Drop Alert!\n\n{message}'.encode('utf-8')
-#     )
-#     connection.close()
+if current_price <= DESIRED_PRICE:
+    # proxy_client = TwilioHttpClient()
+    # proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    # client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages.create(
+        body=f"Price of Gold,is now ₹{current_price}.\n Buy Now!",
+        from_='+19206682105',
+        to='+91 80726 99523'
+    )
+    print(message.status)
